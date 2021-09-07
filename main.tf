751929737e070e8065311109f2d6fd281aed18a3
Christian Nuss <christian.nuss@gmail.com>
2021-09-07 13:39:29 -0700

    Remove cloudtrail kms

--- main.tf
+++ main.tf
@@ -105,108 +105,6 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   ]
 }
 
-resource "aws_kms_key" "cloudtrail" {
-  description = "cloudtrail"
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Id": "CloudTrail key policy",
-    "Statement": [
-        {
-            "Sid": "Enable IAM User Permissions",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": [
-                    "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/${var.root_email}",
-                    "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"
-                ]
-            },
-            "Action": "kms:*",
-            "Resource": "*"
-        },
-        {
-            "Sid": "Allow CloudTrail to encrypt logs",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "kms:GenerateDataKey*",
-            "Resource": "*",
-            "Condition": {
-                "StringLike": {
-                    "kms:EncryptionContext:aws:cloudtrail:arn": "arn:${data.aws_partition.current.partition}:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"
-                }
-            }
-        },
-        {
-            "Sid": "Allow CloudTrail to describe key",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "kms:DescribeKey",
-            "Resource": "*"
-        },
-        {
-            "Sid": "Allow principals in the account to decrypt log files",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "*"
-            },
-            "Action": [
-                "kms:Decrypt",
-                "kms:ReEncryptFrom"
-            ],
-            "Resource": "*",
-            "Condition": {
-                "StringEquals": {
-                    "kms:CallerAccount": "${data.aws_caller_identity.current.account_id}"
-                },
-                "StringLike": {
-                    "kms:EncryptionContext:aws:cloudtrail:arn": "arn:${data.aws_partition.current.partition}:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"
-                }
-            }
-        },
-        {
-            "Sid": "Allow alias creation during setup",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "*"
-            },
-            "Action": "kms:CreateAlias",
-            "Resource": "*",
-            "Condition": {
-                "StringEquals": {
-                    "kms:CallerAccount": "${data.aws_caller_identity.current.account_id}",
-                    "kms:ViaService": "ec2.${data.aws_region.current.name}.amazonaws.com"
-                }
-            }
-        },
-        {
-            "Sid": "Enable cross account log decryption",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "*"
-            },
-            "Action": [
-                "kms:Decrypt",
-                "kms:ReEncryptFrom"
-            ],
-            "Resource": "*",
-            "Condition": {
-                "StringEquals": {
-                    "kms:CallerAccount": "${data.aws_caller_identity.current.account_id}"
-                },
-                "StringLike": {
-                    "kms:EncryptionContext:aws:cloudtrail:arn": "arn:${data.aws_partition.current.partition}:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"
-                }
-            }
-        }
-    ]
-}
-EOF
-}
-
 resource "aws_cloudtrail" "cloudtrail" {
   name                          = "cloudtrail"
   enable_logging                = true
@@ -215,7 +113,6 @@ resource "aws_cloudtrail" "cloudtrail" {
   enable_log_file_validation    = true
 
   s3_bucket_name = aws_s3_bucket.logs.id
-  kms_key_id     = aws_kms_key.cloudtrail.arn
 
   event_selector {
     read_write_type           = "All"
